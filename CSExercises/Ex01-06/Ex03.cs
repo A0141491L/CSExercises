850ed717ba9bb3cf0bf22eb21cf04f5c9e7653fd
A0141491L <E0005458@u.nus.edu>
2015-08-16 15:41:02 +0800

    finish3

--- CSExercises/Ex01-06/Ex03.cs
+++ CSExercises/Ex01-06/Ex03.cs
@@ -10,7 +10,12 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input an number:");
+            string a = (Console.ReadLine());
+            double a1 = Convert.ToDouble(a);
+            double b = a1 * a1;
+            Console.WriteLine(b);
+            Console.ReadLine();
         }
     }
 }
