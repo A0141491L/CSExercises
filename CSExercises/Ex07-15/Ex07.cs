bff307a04c7c98cdfa9f3d899544a3d020ea5c07
A0141491L <E0005458@u.nus.edu>
2015-08-16 17:06:41 +0800

    finish7

--- CSExercises/Ex07-15/Ex07.cs
+++ CSExercises/Ex07-15/Ex07.cs
@@ -10,7 +10,17 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input your salary:");
+            String a1= Console.ReadLine();
+            int a =Convert.ToInt32 (a1);
+
+            double b = 0.1 * a;
+            double c = 0.03 * a;
+            double d = b + c;
+            Console.WriteLine("Your housing allowance is {0}:",b);
+            Console.WriteLine("Your traffic allowance is {0}:",c);
+            Console.WriteLine("Your total income is {0}:",d);
+            Console.ReadLine();
         }
     }
 }
