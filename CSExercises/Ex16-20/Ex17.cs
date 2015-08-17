a74f5f752105f1b27949c9084caa0fd2e184b293
A0141491L <E0005458@u.nus.edu>
2015-08-17 16:10:24 +0800

    finish

--- CSExercises/Ex16-20/Ex17.cs
+++ CSExercises/Ex16-20/Ex17.cs
@@ -4,7 +4,41 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input your name: ");
+            Console.WriteLine("Please input your gender (M or F):");
+            Console.WriteLine("Please input your age: ");
+
+            string name = Console.ReadLine();
+            string gender = Console.ReadLine();
+            int age = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (gender == "M")
+            {
+                if (age >= 40)
+                    Console.WriteLine("GoodMorning Uncle" + name);
+                else
+                {
+                    Console.WriteLine("GoodMorning Mr." + name);
+                }
+            }
+            else if (gender == "F")
+            {
+                if (age >= 40)
+                {
+                    Console.WriteLine("GoodMorning Aunty" + name);
+                }
+
+                else
+                {
+                    Console.WriteLine("GoodMorning Ms." + name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("gender is incorrect");
+            } 
         }
     }
 }
--- CSExercises/Ex16-20/Ex18.cs
+++ CSExercises/Ex16-20/Ex18.cs
@@ -4,7 +4,21 @@ namespace CSExercises
     {
         public static void Main(string[] args)
         {
-            
+            Console.WriteLine("Please input your mark:");
+            int mark = Convert.ToInt32(Console.ReadLine());
+            if (mark < 0 || mark > 100)
+            { Console.WriteLine("Error."); }
+            else if (mark >= 80 && mark <= 100)
+            { Console.WriteLine("You scored {0} marks which is A grade.", mark); }
+            else if (mark >= 60)
+            { Console.WriteLine("You scored {0} marks which is B grade.", mark); }
+            else if (mark >= 40)
+            { Console.WriteLine("You scored {0} marks which is C grade.", mark); }
+
+            else
+            {
+                Console.WriteLine("You scored {0} marks which is F grade.", mark);
+            } 
         }
     }
 }
